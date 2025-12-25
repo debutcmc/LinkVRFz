d08a604d9e81b1112227ff4b45b14fcf20e9a7b7
debutcmc <officialdebutcmc@gmail.com>
2025-12-26 01:06:00 +0700

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,18 @@
+import { initAuthGuard, logout } from "./auth.js";
+
+/* ============================= */
+/* GUARD → WAJIB LOGIN */
+/* ============================= */
+initAuthGuard(true);
+
+/* ============================= */
+/* LOGOUT BUTTON */
+/* ============================= */
+document.getElementById("logoutBtn").onclick = () => {
+  logout()
+    .then(() => {
+      window.location.href = "/login/";
+    })
+    .catch(alert);
+};
 
