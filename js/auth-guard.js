ec7a8db28f21b1f0b4c14a1b9dcca5dadd33c935
debutcmc <officialdebutcmc@gmail.com>
2025-12-27 10:35:57 +0700

    Update auth-guard.js

--- js/auth-guard.js
+++ js/auth-guard.js
@@ -1,10 +1,26 @@
 import { auth } from "./firebase.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { onAuthStateChanged } from
+  "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
 const authBlock = document.getElementById("authBlock");
+const createForm = document.getElementById("createForm");
 
-onAuthStateChanged(auth, (user) => {
-  if (!user) {
-    authBlock.classList.remove("hidden");
-  }
-});
+if (authBlock) {
+  onAuthStateChanged(auth, (user) => {
+    if (!user) {
+      // USER BELUM LOGIN
+      authBlock.classList.remove("hidden");
+
+      if (createForm) {
+        createForm.classList.add("disabled");
+      }
+    } else {
+      // USER LOGIN
+      authBlock.classList.add("hidden");
+
+      if (createForm) {
+        createForm.classList.remove("disabled");
+      }
+    }
+  });
+}
